7dc63fe6f85b65d7601dd1ec3ca12237ddb92572
Gavin Quirk <gavinquirk@gmail.com>
2018-07-17 15:58:56 -0700

    fixed landing page config file error

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,7 +5,7 @@ var locNameArray = []
 var breweryIdArray = []
 var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'I1', 'J1', 'K', 'L1', 'M1', 'N1', 'O1', 'P1', 'Q1', 'R1', 'S1', 'T1', 'U1', 'V1', 'W1', 'X1', 'Y1', 'Z1']
 var labelIndex = 0
-var apiKey = config.API_KEY;
+var apiKey = config.API_KEY
 
 //On click for landing page modal
 $("#enterButton").on('click', function () {
